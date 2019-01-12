d8800a88a89fe4e3f384386dbba607f7c7f4e758
Jens Gatzweiler <lichthagel@tuta.io>
2019-01-12 17:59:04 +0100

    Basic stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+struct TraktApi {
+    client: reqwest::Client,
+    client_id: String,
+    client_secret: String
+}
+
+impl TraktApi {
+    fn new(client_id: String, client_secret: String) -> TraktApi {
+        TraktApi {
+            client: reqwest::Client::new(),
+            client_id,
+            client_secret
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
