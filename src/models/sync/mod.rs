accc27ee47933d5f93cf1fc2e432b62c947e788f
Jens Gatzweiler <lichthagel@tuta.io>
2019-01-20 20:19:04 +0100

    extracted sync stuff

---
+++ src/models/sync/mod.rs
@@ -0,0 +1,75 @@
+pub mod sync_request;
+
+pub use crate::models::sync::sync_request::{
+    SyncFactory, SyncSeasonFactory, SyncShowFactory, SyncType,
+};
+
+use crate::models::{
+    Episode, Movie, OptionEpisode, OptionMovie, OptionSeason, OptionShow, Season, Show,
+    WatchableType,
+};
+use chrono::{DateTime, Utc};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LastActivities {
+    all: DateTime<Utc>,
+    movies: LastActivitiesElement,
+    episodes: LastActivitiesElement,
+    shows: LastActivitiesElement,
+    seasons: LastActivitiesElement,
+    comments: LastActivitiesElement,
+    lists: LastActivitiesElement,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LastActivitiesElement {
+    watched_at: Option<DateTime<Utc>>,
+    collected_at: Option<DateTime<Utc>>,
+    rated_at: Option<DateTime<Utc>>,
+    watchlisted_at: Option<DateTime<Utc>>,
+    commented_at: Option<DateTime<Utc>>,
+    paused_at: Option<DateTime<Utc>>,
+    hidden_at: Option<DateTime<Utc>>,
+    liked_at: Option<DateTime<Utc>>,
+    updated_at: Option<DateTime<Utc>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Playback {
+    progress: f64,
+    paused_at: Option<DateTime<Utc>>,
+    id: u64,
+    #[serde(rename = "type")]
+    item_type: WatchableType,
+    movie: Option<Movie>,
+    episode: Option<Episode>,
+    show: Option<Show>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SyncAddResponse {
+    added: SyncResponseNumbers,
+    updated: Option<SyncResponseNumbers>,
+    existing: Option<SyncResponseNumbers>,
+    not_found: SyncResponseNotFound,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SyncRemoveResponse {
+    deleted: SyncResponseNumbers,
+    not_found: SyncResponseNotFound,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SyncResponseNumbers {
+    movies: u32,
+    episodes: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SyncResponseNotFound {
+    movies: Vec<OptionMovie>,
+    shows: Vec<OptionShow>,
+    seasons: Vec<OptionSeason>,
+    episodes: Vec<OptionEpisode>,
+}
--- src/models/sync/sync_request.rs
+++ src/models/sync/sync_request.rs
@@ -1,49 +1,10 @@
 use crate::{
     error::{Error, Result},
-    models::{
-        Episode, Movie, OptionEpisode, OptionMovie, OptionSeason, OptionShow, Season, Show,
-        WatchableType,
-    },
+    models::{Episode, Movie, Season, Show},
 };
 use chrono::{DateTime, Utc};
 use serde_json::{Map, Value};
 use std::collections::HashMap;
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct LastActivities {
-    all: DateTime<Utc>,
-    movies: LastActivitiesElement,
-    episodes: LastActivitiesElement,
-    shows: LastActivitiesElement,
-    seasons: LastActivitiesElement,
-    comments: LastActivitiesElement,
-    lists: LastActivitiesElement,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct LastActivitiesElement {
-    watched_at: Option<DateTime<Utc>>,
-    collected_at: Option<DateTime<Utc>>,
-    rated_at: Option<DateTime<Utc>>,
-    watchlisted_at: Option<DateTime<Utc>>,
-    commented_at: Option<DateTime<Utc>>,
-    paused_at: Option<DateTime<Utc>>,
-    hidden_at: Option<DateTime<Utc>>,
-    liked_at: Option<DateTime<Utc>>,
-    updated_at: Option<DateTime<Utc>>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Playback {
-    progress: f64,
-    paused_at: Option<DateTime<Utc>>,
-    id: u64,
-    #[serde(rename = "type")]
-    item_type: WatchableType,
-    movie: Option<Movie>,
-    episode: Option<Episode>,
-    show: Option<Show>,
-}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum SyncType {
@@ -504,32 +465,4 @@ impl SyncSeasonFactory {
         self.episodes.insert(episode, Some(collected_at));
         self
     }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct SyncAddResponse {
-    added: SyncResponseNumbers,
-    updated: Option<SyncResponseNumbers>,
-    existing: Option<SyncResponseNumbers>,
-    not_found: SyncResponseNotFound,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct SyncRemoveResponse {
-    deleted: SyncResponseNumbers,
-    not_found: SyncResponseNotFound,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct SyncResponseNumbers {
-    movies: u32,
-    episodes: u32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct SyncResponseNotFound {
-    movies: Vec<OptionMovie>,
-    shows: Vec<OptionShow>,
-    seasons: Vec<OptionSeason>,
-    episodes: Vec<OptionEpisode>,
 }
