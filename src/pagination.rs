4e3138ff4a13d9bd6b7abd0d4704b260cd44d4fc
Jens Gatzweiler <lichthagel@tuta.io>
2019-01-21 10:43:18 +0100

    Add Pagination trait

--- src/pagination.rs
+++ src/pagination.rs
@@ -1,24 +1,26 @@
+pub trait Pagination {
+    fn page(self, page: u32) -> Self;
+    fn limit(self, limit: u32) -> Self;
+}
+
 pub struct PaginationFactory {
     pub page: u32,
-    pub limit: u32
+    pub limit: u32,
 }
 
 impl Default for PaginationFactory {
     fn default() -> Self {
-        Self {
-            page: 1,
-            limit: 10
-        }
+        Self { page: 1, limit: 10 }
     }
 }
 
-impl PaginationFactory {
-    pub fn page(mut self, page: u32) -> Self {
+impl Pagination for PaginationFactory {
+    fn page(mut self, page: u32) -> Self {
         self.page = page;
         self
     }
 
-    pub fn limit(mut self, limit: u32) -> Self {
+    fn limit(mut self, limit: u32) -> Self {
         self.limit = limit;
         self
     }
