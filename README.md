86d77652181088abbea2bb440044e05cc08e4eb1
Jens Gatzweiler <lichthagel@tuta.io>
2019-02-05 17:54:47 +0100

    Add documentation to lib.rs and update README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # trakt-rust
-A rust wrapper for the trakt.tv api
+
+A api wrapper in Rust for trakt.tv
+
+## Installation
+
+Not yet released but you may add
+```toml
+trakt = { git = "https://github.com/Lichthagel/trakt-rust" }
+```
+to your dependencies
+
+## Examples
+
+TODO
+
+## License
+
+[MIT][license]
+
+[license]: https://github.com/Lichthagel/trakt-rust/blob/master/LICENSE
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! # trakt-rust
+//!
+//! A api wrapper in Rust for trakt.tv
+//!
+//! ## Installation
+//!
+//! Not yet released but you may add
+//! ```toml
+//! trakt = { git = "https://github.com/Lichthagel/trakt-rust" }
+//! ```
+//! to your dependencies
+//!
+//! ## Examples
+//!
+//! TODO
+//!
+//! ## License
+//!
+//! [MIT][license]
+//!
+//! [license]: https://github.com/Lichthagel/trakt-rust/blob/master/LICENSE
+
 #[macro_use]
 extern crate serde_derive;
 extern crate chrono;
@@ -19,6 +41,7 @@ use crate::{
 };
 use serde::de::DeserializeOwned;
 
+/// The main struct which contains all requests
 #[derive(Debug, Clone)]
 pub struct TraktApi {
     client: reqwest::Client,
@@ -26,7 +49,9 @@ pub struct TraktApi {
     client_secret: Option<String>,
 }
 
+/// Generic functions and simple requests
 impl TraktApi {
+    /// Creates a new client client ID is needed client secret is optional if you need authorization
     pub fn new(client_id: String, client_secret: Option<String>) -> TraktApi {
         TraktApi {
             client: reqwest::Client::new(),
@@ -35,10 +60,36 @@ impl TraktApi {
         }
     }
 
+    /// A generic function which makes a GET request to the given url and receives a deserialized Object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn get<T: DeserializeOwned>(&self, url: String) -> Result<T> {
         self._get(&url)
     }
 
+    /// A generic function which makes a GET request to the given url and receives a deserialized Object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn _get<T: DeserializeOwned>(&self, url: &str) -> Result<T> {
         match self
             .client
@@ -59,10 +110,36 @@ impl TraktApi {
         }
     }
 
+    /// A generic function which makes an authorized GET request to the given url and receives a deserialized Object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn auth_get<T: DeserializeOwned>(&self, url: String, access_token: &str) -> Result<T> {
         self._auth_get(&url, access_token)
     }
 
+    /// A generic function which makes an authorized GET request to the given url and receives a deserialized Object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn _auth_get<T: DeserializeOwned>(&self, url: &str, access_token: &str) -> Result<T> {
         match self
             .client
@@ -84,10 +161,36 @@ impl TraktApi {
         }
     }
 
+    /// A generic function which makes a POST request to the given url and receives a deserialized Object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn post<T: DeserializeOwned>(&self, url: String, body: String) -> Result<T> {
         self._post(&url, body)
     }
 
+    /// A generic function which makes a POST request to the given url and receives a deserialized Object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn _post<T: DeserializeOwned>(&self, url: &str, body: String) -> Result<T> {
         match self
             .client
@@ -109,6 +212,19 @@ impl TraktApi {
         }
     }
 
+    /// A generic function which makes an authorized POST request to the given url and receives a deserialized Object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn auth_post<T: DeserializeOwned>(
         &self,
         url: String,
@@ -118,6 +234,19 @@ impl TraktApi {
         self._auth_post(&url, body, access_token)
     }
 
+    /// A generic function which makes an authorized POST request to the given url and receives a deserialized Object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn _auth_post<T: DeserializeOwned>(
         &self,
         url: &str,
@@ -145,10 +274,36 @@ impl TraktApi {
         }
     }
 
+    /// A generic function which makes an authorized POST request to the given url and receives nothing
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn auth_post_no_body(&self, url: String, body: String, access_token: &str) -> Result<()> {
         self._auth_post_no_body(&url, body, access_token)
     }
 
+    /// A generic function which makes an authorized POST request to the given url and receives nothing
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn _auth_post_no_body(&self, url: &str, body: String, access_token: &str) -> Result<()> {
         match self
             .client
@@ -171,6 +326,19 @@ impl TraktApi {
         }
     }
 
+    /// A generic function which makes an authorized PUT request to the given url and receives a deserialized object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn auth_put<T: DeserializeOwned>(
         &self,
         url: String,
@@ -180,6 +348,19 @@ impl TraktApi {
         self._auth_put(&url, body, access_token)
     }
 
+    /// A generic function which makes an authorized PUT request to the given url and receives a deserialized object
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn _auth_put<T: DeserializeOwned>(
         &self,
         url: &str,
@@ -207,10 +388,36 @@ impl TraktApi {
         }
     }
 
+    /// A generic function which makes an authorized DELETE request to the given url and receives nothing
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn auth_delete(&self, url: String, access_token: &str) -> Result<()> {
         self._auth_delete(&url, access_token)
     }
 
+    /// A generic function which makes an authorized DELETE request to the given url and receives nothing
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
     fn _auth_delete(&self, url: &str, access_token: &str) -> Result<()> {
         match self
             .client
@@ -232,22 +439,92 @@ impl TraktApi {
         }
     }
 
+    /// Get a Vec of all certifications, including names, slugs, and descriptions. [Trakt API][more]
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
+    /// [more]: https://trakt.docs.apiary.io/#reference/certifications/list/get-certifications
     pub fn certifications(&self, ct: CertificationsType) -> Result<Certifications> {
         self.get(api_url!(("certifications", ct.to_string())))
     }
 
+    /// Get a Vec of all countries, including names and codes. [Trakt API][more]
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
+    /// [more]: https://trakt.docs.apiary.io/#reference/countries/list/get-countries
     pub fn countries(&self, media_type: MediaType) -> Result<Vec<Country>> {
         self.get(api_url!(("countries", media_type.to_string())))
     }
 
+    /// Get a Vec of all genres, including names and slugs. [Trakt API][more]
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
+    /// [more]: https://trakt.docs.apiary.io/#reference/genres/get-genres
     pub fn genres(&self, media_type: MediaType) -> Result<Vec<Genre>> {
         self.get(api_url!(("genres", media_type.to_string())))
     }
 
+    /// Get a Vec of all langauges, including names and codes. [Trakt API][more]
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
+    /// [more]: https://trakt.docs.apiary.io/#reference/genres/list/get-languages
     pub fn languages(&self, media_type: MediaType) -> Result<Vec<Language>> {
         self.get(api_url!(("languages", media_type.to_string())))
     }
 
+    /// Get a Vec of all TV networks, including the name. [Trakt API][more]
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Response`] if the response contains an unsuccessful status code
+    ///
+    /// Returns [`Error::Connection`] if the connection failed
+    ///
+    /// Returns [`Error::Serde`] if the response could not be deserialized
+    ///
+    /// [`Error::Response`]: error/enum.Error.html#variant.Response
+    /// [`Error::Connection`]: error/enum.Error.html#variant.Connection
+    /// [`Error::Serde`]: error/enum.Error.html#variant.Serde
+    /// [more]: https://trakt.docs.apiary.io/#reference/networks/list/get-networks
     pub fn networks(&self) -> Result<Vec<Network>> {
         self.get(api_url!(("networks")))
     }
