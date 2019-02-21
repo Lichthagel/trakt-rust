6d9dc771d2a331c6992198f20360896ad8a34bc4
Jens Gatzweiler <lichthagel@tuta.io>
2019-02-21 19:45:00 +0100

    Added docs to filter.rs

--- src/filters.rs
+++ src/filters.rs
@@ -1,5 +1,8 @@
 use crate::models::ShowStatus;
 
+/// Trait for applying different common filters. [More]
+///
+/// [More]: https://trakt.docs.apiary.io/#introduction/filters
 pub trait CommonFilters {
     fn query(self, query: &str) -> Self;
     fn year(self, year: u32) -> Self;
@@ -10,16 +13,23 @@ pub trait CommonFilters {
     fn ratings(self, from: u32, to: u32) -> Self;
 }
 
+/// Trait for applying movie filters. [More]
+///
+/// [More]: https://trakt.docs.apiary.io/#introduction/filters
 pub trait MovieFilters {
     fn certification(self, cert_slug: &str) -> Self;
 }
 
+/// Trait for applying show filters. [More]
+///
+/// [More]: https://trakt.docs.apiary.io/#introduction/filters
 pub trait ShowFilters {
     fn certification(self, cert_slug: &str) -> Self;
     fn network(self, network_name: &str) -> Self;
     fn status(self, status: ShowStatus) -> Self;
 }
 
+/// Trait for filtering by media types, etc.
 pub trait TypeFilter<T> {
     fn item_type(self, item_type: T) -> Self;
 }
