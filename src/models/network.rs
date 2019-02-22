a4acfa0e080e0ab28303a593f255cec1711d5553
Jens Gatzweiler <lichthagel@tuta.io>
2019-02-22 16:25:36 +0100

    Add docs to models/network.rs

--- src/models/network.rs
+++ src/models/network.rs
@@ -1,3 +1,6 @@
+/// A [network]
+///
+/// [network]: https://trakt.docs.apiary.io/#reference/networks
 #[derive(Debug, Serialize, Deserialize, Ord, PartialOrd, Eq, PartialEq)]
 pub struct Network {
     pub name: String,
