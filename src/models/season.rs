b05cbb54f88cd57d8db369362f728367c76d0794
Jens Gatzweiler <lichthagel@tuta.io>
2019-02-22 16:46:25 +0100

    Move Person to models/season.rs

--- src/models/season.rs
+++ src/models/season.rs
@@ -4,12 +4,19 @@ use crate::{
 };
 use chrono::{DateTime, Utc};
 
+/// A [season]
+///
+/// [season]: https://trakt.docs.apiary.io/#reference/seasons
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Season {
     pub number: u32,
     pub ids: Ids,
 }
 
+/// A [season] with full [extended info]
+///
+/// [season]: https://trakt.docs.apiary.io/#reference/seasons
+/// [extended info]: https://trakt.docs.apiary.io/#introduction/extended-info
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FullSeason {
     pub number: u32,
@@ -32,6 +39,9 @@ impl WithNone for FullSeason {
     type None = Season;
 }
 
+/// A [Season] with only optional fields
+///
+/// [Season]: struct.Season.html
 #[derive(Debug, Serialize, Deserialize)]
 pub struct OptionSeason {
     #[serde(skip_serializing_if = "Option::is_none")]
