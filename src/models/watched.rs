ae2368c38e58bddb95b90beb3793e528719d84f9
Jens Gatzweiler <lichthagel@tuta.io>
2019-02-22 17:03:53 +0100

    Move Person to models/watched.rs

--- src/models/watched.rs
+++ src/models/watched.rs
@@ -1,6 +1,9 @@
 use crate::models::{Movie, Show};
 use chrono::{DateTime, Utc};
 
+/// A [watched entry] of an user
+///
+/// [watched entry]: https://trakt.docs.apiary.io/#reference/users/watched/get-watched
 #[derive(Debug, Serialize, Deserialize)]
 pub struct WatchedEntry {
     pub plays: u32,
@@ -11,12 +14,18 @@ pub struct WatchedEntry {
     pub seasons: Option<Vec<WatchedSeason>>,
 }
 
+/// A watched season in [WatchedEntry]
+///
+/// [WatchedEntry]: struct.WatchedEntry.html
 #[derive(Debug, Serialize, Deserialize)]
 pub struct WatchedSeason {
     pub number: u32,
     pub episodes: Vec<WatchedEpisode>,
 }
 
+/// A watched episode in [WatchedSeason]
+///
+/// [WatchedSeason]: struct.WatchedSeason.html
 #[derive(Debug, Serialize, Deserialize)]
 pub struct WatchedEpisode {
     pub number: u32,
