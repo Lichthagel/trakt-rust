170d346bdf11f7c7b9f7b2a5d8872bff97a78a66
Jens Gatzweiler <lichthagel@tuta.io>
2019-02-22 15:58:01 +0100

    Add docs to models/genre.rs

--- src/models/genre.rs
+++ src/models/genre.rs
@@ -1,3 +1,6 @@
+/// A [genre]
+///
+/// [genre]: https://trakt.docs.apiary.io/#reference/genres
 #[derive(Debug, Serialize, Deserialize, Ord, PartialOrd, Eq, PartialEq)]
 pub struct Genre {
     pub name: String,
